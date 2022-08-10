1f4423fd08afd6f729d5d32be7c5f7321e9a9179
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-10 20:44:59 +0500

    add specs, readme, design

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,7 +266,7 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -308,4 +308,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 end
